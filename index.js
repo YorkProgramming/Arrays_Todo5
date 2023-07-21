8207cfb0e187cca1085f6f72a3a3d5b676ef0729
Matthew York <myorkprogramming@gmail.com>
2023-07-21 12:18:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//1.
+//Average (Warmup)
+
+/* (Warm-up) Always run through some quick algorithm problems before any coding interview, 
+to get yourself warmed up. How about this one: return the average value of an array of unsorted numbers. */
